6033b9af756b2bcef5a4523e714063fb6233beb9
Cyxo <paulolol.jeu@gmail.com>
2024-01-05 13:15:05 +0100

    Add docstrings to ABaseGato and comments to ExampleGato

--- gato/gatos/ABaseGato.py
+++ gato/gatos/ABaseGato.py
@@ -3,6 +3,7 @@ from random import random
 
 
 def require_alive(function):
+    """Decorator that only executes a function if the gato has not fainted. **Warning:** Don't add that to functions that shouldn't return `None`."""
     def new_function(gato: "ABaseGato", *args, **kwargs):
         if not gato._fainted:
             return function(gato, *args, **kwargs)
@@ -12,37 +13,32 @@ def require_alive(function):
 
 
 class ABaseGato(ABC):
+    """**Abstract class** to implement for every gato.
 
-    max_mood: float = 100.0
-    max_hunger: float = 100.0
-    max_energy: float = 100.0
-    max_health: float = 100.0
-
-    mood: float
-    hunger: float
-    energy: float
-    health: float
-
-    base_efficiency: float = 1.0
-    luck: float = 1.0
-    efficiency_boost: float = 0.0
-    friendship: float = 1.0
-    deployed_today: int = 0
-
-    _fainted: bool = False
-    _events: list[dict] = []
-    _time_deployed: int = 0
-
-    name: str
-    eidolon: int
+    Attributes starting with a `_` should not be modified manually.
+    Attributes in CAPS are constants."""
 
     IMAGE: str = "https://cdn.discordapp.com/emojis/1173895764087414855.webp"
+    """Sprite of the gato. **OVERRIDE IT!**"""
+
+    ANIMATIONS: str = "mooncake"
+    """A reference to a key in `animations.json`. **OVERRIDE IT!**"""
+
+    DISPLAY_NAME: str = "Base Gato"
+    """Display name of this gato class. **OVERRIDE IT!**"""
+
     EVENT_DESCRIPTIONS = {
         "fainted": "fainted.",
         "bitten": "is angry and bites you (x{count}). You lose **{amount}** {currency} in total"
     }
+    """Description for the gato's events. *Can be overriden or completed with custom events.*
+    For example, `SkellyGato` has a custom event called `"resurrect"` with a custom description."""
+
     BASE_EARN_RATE: float = 0.25
+    """Number of currency per second the gato earns. **DON'T OVERRIDE IT**, rather override :py:attr:`base_efficiency`."""
+
     BITE_CHANCE: float = 1/100
+    """Base chance to get bitten."""
 
     VALUES_TO_SAVE = [
         "name",
@@ -56,9 +52,73 @@ class ABaseGato(ABC):
         "friendship",
         "deployed_today"
     ]
+    """Attributes that will be saved when exporting the gato to JSON. *Can be overriden or completed with custom attributes.*"""
+
+
+    max_mood: float = 100.0
+    """Maximum value for the :py:attr:`mood` stat. *You can override that.*"""
+
+    max_hunger: float = 100.0
+    """Maximum value for the :py:attr:`hunger` stat. *You can override that.*"""
+
+    max_energy: float = 100.0
+    """Maximum value for the :py:attr:`energy` stat. *You can override that.*"""
+
+    max_health: float = 100.0
+    """Maximum value for the :py:attr:`health` stat. *You can override that.*"""
+
+
+    mood: float
+    """Current value for the :py:attr:`mood` stat. Chances to get bitten when :py:attr:`mood` is low. Updated in real time by the :py:meth:`lose_stats_over_time` method when deployed."""
+
+    hunger: float
+    """Current value for the :py:attr:`hunger` stat. Loses health when :py:attr:`hunger` is low. Updated in real time by the :py:meth:`lose_stats_over_time` method when deployed."""
+
+    energy: float
+    """Current value for the :py:attr:`energy` stat. Reduces efficiency when :py:attr:`energy` is low. Updated in real time by the :py:meth:`lose_stats_over_time` method when deployed."""
+
+    health: float
+    """Current value for the :py:attr:`health` stat. Faints when :py:attr:`health` reaches **0**. Updated in real time by the :py:meth:`lose_stats_over_time` method when deployed."""
+
+
+    efficiency: float
+    """Current value for efficiency stat. Updated in real time by the :py:meth:`compute_currency` method when deployed."""
+
+    base_efficiency: float = 1.0
+    """Base efficiency stat. *You can override that.*"""
+
+    luck: float = 1.0
+    """Multiplier for the chance to find treasures. *You can override that.*"""
+
+    efficiency_boost: float = 0.0
+    """Boost of the efficiency stat. Can be affected by the gato or other gatos."""
+
+    friendship: float = 1.0
+    """Friendship stat, starts at 1, max 10."""
+
+    claimed_today: int = 0
+    """Number of times the rewards of this gato have been claimed today."""
+
+
+    _fainted: bool = False
+    """Becomes :keyword:`True` if the gato's health is 0. Updated by :py:meth:`add_health`"""
+
+    _events: list[dict] = []
+    """List of events. Cleared everytime the rewards are claimed. An event is a dict like `{"event_name": "optional value (can be any type)"}`"""
+
+    _time_deployed: int = 0
+    """Number of seconds since the gato has been deployed. Updated in real-time by :py:meth:`simulate`"""
+
+
+    name: str
+    """Name of the gato. Choosen by the owner."""
+
+    eidolon: int
+    """Eidolon level of the gato. Starts at 0, max 6."""
 
 
     def __init__(self, **kwargs):
+        """Constructor."""
         self.mood = self.max_mood
         self.hunger = self.max_hunger
         self.energy = self.max_energy
@@ -75,6 +135,11 @@ class ABaseGato(ABC):
             self._fainted = True
 
     def to_json(self):
+        """Exports a gato to JSON.
+
+        :return: A dict containing the gato's class name, and all the values specified in :py:attr:`VALUES_TO_SAVE`.
+        :rtype: dict
+        """
         return {
             "type": self.__class__.__name__,
             "values": dict((val, getattr(self, val)) for val in self.VALUES_TO_SAVE)
@@ -82,21 +147,38 @@ class ABaseGato(ABC):
 
     @classmethod
     def from_json(cls, json: dict):
+        """Class method to import a gato from JSON.
+
+        :classmethod:
+        :param json: A dict exported by :py:meth:`to_json`.
+        :type json: dict
+        :return: The imported gato
+        :rtype: :py:class:`ABaseGato`
+        """
         return cls(**json["values"])
 
 
     def deploy(self):
-        self._events = []
+        """Called when a gato is deployed."""
+
         self._time_deployed = 0
+        self.efficiency_boost = 0.0
 
         if self.health > 0:
             self._fainted = False
 
-        if self.friendship < 10 and self.deployed_today < 5:
+    def claim(self):
+        """Called everytime the owner claims the rewards of this gato."""
+        self._events = []
+
+        if self.friendship < 10 and self.claimed_today < 5:
             self.friendship += 0.1
+
+        self.claimed_today += 1
 
 
     def lose_stats_over_time(self, seconds):
+        """Computes stat loss over time. Called by :py:meth:`simulate`. *Can be overriden.*"""
         self.add_hunger(-0.01 * seconds)
         self.add_energy(-0.02 * seconds)
 
@@ -112,6 +194,7 @@ class ABaseGato(ABC):
 
 
     def compute_currency(self, seconds):
+        """Computes efficiency and currency gain over time. Called by :py:meth:`simulate`. *Can be overriden.*"""
         self.efficiency = self.base_efficiency + self.efficiency_boost
         if self.energy < 10:
             self.efficiency -= 0.2
@@ -122,6 +205,7 @@ class ABaseGato(ABC):
 
 
     def random_object(self, seconds):
+        """Computes random reward findings. Called by :py:meth:`simulate`. *Can be overriden.*"""
         objects = []
         if self._time_deployed % 60 == 0 and self.efficiency >= 1:
             if random() < self.luck / 100:
@@ -132,7 +216,19 @@ class ABaseGato(ABC):
 
     @abstractmethod
     def simulate(self, team: list["ABaseGato"], seconds: int = 1):
-        if self.health > 0:
+        """Runs a simulation of the gato over a certain amount of time.
+        Abstract method, **needs to be overriden**. Overriding method should still call superclass method (see :py:class:`ExampleGato`).
+        Returns 0-values when gato is fainted.
+
+        :param team: The team the gato is deployed in. Can be used for team-based buffs.
+        :type team: list[:py:class:`ABaseGato`]
+        :param seconds: Simulation duration in seconds, defaults to 1.
+        :type seconds: int, optional
+        :return: The amount of currency and the list of objects gathered by the gato.
+        :rtype: tuple[float, list[str]]
+        """
+
+        if not self._fainted:
             self._time_deployed += seconds
 
             self.lose_stats_over_time(seconds)
@@ -148,6 +244,17 @@ class ABaseGato(ABC):
 
     @require_alive
     def add_health(self, amount: float):
+        """Affect a gato's health within its limits.
+        Amount can be negative.
+        Use this rather than modifying :py:attr:`health` directly.
+        Doesn't work when gato is fainted.
+        Also sets :py:attr:`_fainted` when :py:attr:`health` is 0.
+        *Can be overriden.*
+
+        :param amount: Amount of health to add.
+        :type amount: float
+        """
+
         self.health += amount
 
         if self.health > self.max_health:
@@ -160,6 +267,16 @@ class ABaseGato(ABC):
 
     @require_alive
     def add_mood(self, amount: float):
+        """Affect a gato's mood within its limits.
+        Amount can be negative.
+        Use this rather than modifying :py:attr:`mood` directly.
+        Doesn't work when gato is fainted.
+        *Can be overriden.*
+
+        :param amount: Amount of mood to add.
+        :type amount: float
+        """
+
         self.mood += amount
 
         if self.mood > self.max_mood:
@@ -170,6 +287,16 @@ class ABaseGato(ABC):
 
     @require_alive
     def add_hunger(self, amount: float):
+        """Affect a gato's hunger within its limits.
+        Amount can be negative.
+        Use this rather than modifying :py:attr:`hunger` directly.
+        Doesn't work when gato is fainted.
+        *Can be overriden.*
+
+        :param amount: Amount of hunger to add.
+        :type amount: float
+        """
+
         self.hunger += amount
 
         if self.hunger > self.max_hunger:
@@ -180,6 +307,16 @@ class ABaseGato(ABC):
 
     @require_alive
     def add_energy(self, amount: float):
+        """Affect a gato's energy within its limits.
+        Amount can be negative.
+        Use this rather than modifying :py:attr:`energy` directly.
+        Doesn't work when gato is fainted.
+        *Can be overriden.*
+
+        :param amount: Amount of energy to add.
+        :type amount: float
+        """
+
         self.energy += amount
 
         if self.energy > self.max_energy:
--- gato/gatos/ExampleGato.py
+++ gato/gatos/ExampleGato.py
@@ -9,46 +9,72 @@ class ExampleGato(ABaseGato):
         > Has **1%** (**2%** at **E6**) chance to find a **Rare treasure** each minute.
     """
 
-    base_efficiency: float = 1.1    # Override initial values for stats
+    # Override constants
+    IMAGE: str = "https://cdn.discordapp.com/emojis/1173895764087414855.webp"
+    ANIMATIONS: str = "mooncake"
+    DISPLAY_NAME: str = "Example Gato"
+    VALUES_TO_SAVE = ABaseGato.VALUES_TO_SAVE + [
+        "buff_duration",
+        "buff_cooldown",
+        "has_buff",
+        "find_object_cooldown"
+    ]
 
+    # Override superclass values for stats
+    base_efficiency: float = 1.1
 
-    _buff_duration: int = 0         # Custom variables used for this gato
-    _buff_cooldown: int = 0
-    _has_buff: bool = False
-    _find_object_cooldown: int = 0
+    # Custom variables used for this gato
+    buff_duration: int = 0              # Remaining duration for its buff
+    buff_cooldown: int = 0              # Remaining cooldown until its buff can be triggered again
+    has_buff: bool = False              # Whether the gato currently is under its buff or not
+    find_object_cooldown: int = 0       # Remaining cooldown until it can find a rare object again
 
 
     @require_alive
     def efficiency_buff(self, seconds):
-        self._buff_duration -= seconds
-        self._buff_cooldown -= seconds
+        # Update buff duration and cooldown
+        self.buff_duration -= seconds
+        self.buff_cooldown -= seconds
 
-        if self._buff_cooldown <= 0:
+        # Apply buff if cooldown is over
+        if self.buff_cooldown <= 0:
+            # Increase efficiency boost
             self.efficiency_boost += 20/100 + (2/100 * self.eidolon)
-            self._buff_duration += 20*60
-            self._buff_cooldown += 60*60
-            self._has_buff = True
 
-        if self._buff_duration <= 0 and self._has_buff:
+            # Set base cooldown and duration
+            self.buff_duration += 20*60
+            self.buff_cooldown += 60*60
+            self.has_buff = True
+
+        # Remove buff if duration is over
+        if self.buff_duration <= 0 and self.has_buff:
             self.efficiency_boost -= 20/100 + (2/100 * self.eidolon)
-            self._has_buff = False
+            self.has_buff = False
 
 
     def random_object(self, seconds):
+        # Call superclass method to find base objects
         objects = super().random_object(seconds)
 
-        self._find_object_cooldown -= seconds
+        # Update object finding cooldown
+        self.find_object_cooldown -= seconds
 
-        if self._find_object_cooldown <= 0:
-            self._find_object_cooldown = 60
+        # If the cooldown is over, try to find an object
+        if self.find_object_cooldown <= 0:
+            # Set base cooldown
+            self.find_object_cooldown = 60
 
+            # Calculate chances to find a Rare treasure (see skill description at the top of the class)
             chances = 0.01
             if self.eidolon == 6:
                 chances = 0.02
 
-            if random() < chances:
+            # Randomly find an Rare treasure
+            if random() < self.luck*chances:
+                # If we found one, add it to the objects found by the superclass method
                 objects.append("Rare treasure")
 
+        # Return found objects
         return objects
 
 
@@ -59,5 +85,5 @@ class ExampleGato(ABaseGato):
         # Then call the parent simulation (VERY IMPORTANT)
         currency, objects = super().simulate(seconds)
 
+        # Return gathered currency and objects
         return currency, objects
-
