2b3feb025d8a19dd97f988f8f07fea4ce63a5f5d
Cyxo Cyxo <paulolol.jeu@gmail.com>
2024-02-14 23:56:55 +0000

    Parrot plugin

---
+++ parrot/parrot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+
+
+class MessageCounter:
+
+    def __init__(self):
+        self.message = ""
+        self.count = 0
+        self.users = []
+
+
+class Parrot(commands.Cog):
+    """Funpost Plugin"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.messages: dict[int, MessageCounter] = {}
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.channel.id not in self.messages:
+            self.messages[message.channel.id] = MessageCounter()
+
+        ct = self.messages[message.channel.id]
+        if message.content == ct.message:
+            # if message.author.id not in ct.users:
+                ct.count += 1
+                ct.users.append(message.author.id)
+        else:
+            ct.message = message.content
+            ct.users = [message.author.id]
+            ct.count = 1
+
+        if ct.count == 4:
+            await message.channel.send(ct.message)
+
+
+async def setup(bot):
+    await bot.add_cog(Parrot(bot))
+
